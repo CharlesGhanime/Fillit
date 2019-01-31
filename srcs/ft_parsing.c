d407d8a843e4ece8c73486c35baa44f41f3ec028
CharlesGhanime <ghanime.charles@gmail.com>
2019-01-31 18:39:11 +0100

    parsing in progress, please wait...

--- srcs/ft_parsing.c
+++ srcs/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 16:34:22 by cghanime          #+#    #+#             */
-/*   Updated: 2019/01/29 18:23:49 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/01/31 18:38:04 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,40 @@
 #include "libft/libft.h"
 #include <libc.h>
 
-void	ft_parsing(char *tab, int fd)
+int		ft_parsing(char *tab, int fd)
 {
 	int i;
+	int diese;
 	int ret;
 
 	i = 0;
+	diese = 0;
 	ret = read(fd, tab, 545);
+	while (i != '\0')
 	{
-		if ((tab[i] == '#' && tab[i + 1] == '#') || (tab[i] == '#' && tab[i + 5]))
-			if ((tab[i] == '.' && tab[i - 1] == '#') || (tab[i] == '#' && tab[i + 1] == '.'))
-					i++;
+		if (tab[i] == '#')
+			diese++;
+		if (diese > 4)
+			return (0);
+		if ((tab[i] == '#' && tab[i + 1] == '#') || (tab[i] == '#' && tab[i + (5 - i)] == '#'))
+			if ((tab[i] == '.' && tab[i + (5 - i)] == '#') || (tab[i] == '#'))
+			{
+				printf("test ok");
+				i++;
+			}
 	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int fd;
+	char *tab;
+
+	if (argc > 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		ft_parsing(tab, fd);
+	}
+	return (0);
 }
