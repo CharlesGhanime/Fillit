89860fcaa3c28e3e854bddd8841bd7599c5387c2
CharlesGhanime <ghanime.charles@gmail.com>
2019-02-06 18:25:28 +0100

    parsing en plusieurs fonctions

--- srcs/ft_parsing_2.c
+++ srcs/ft_parsing_2.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 16:34:22 by cghanime          #+#    #+#             */
-/*   Updated: 2019/02/05 19:03:37 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/02/06 18:24:59 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,89 @@
 #include <libc.h>
 #include <unistd.h>
 
-int		ft_parsing(char *tab,		int fd)
+typedef int bool;
+#define true 1
+#define false 0
+
+int check_lines(char *tab, int fd)
 {
 	int i;
-	int diese;
+	int diese_lines;
+
+	i = 0;
+	diese_lines = 0;
+
+	while (tab[i] != '\n')
+	{
+		if (tab[i] != '#' && tab[i] != '.')
+		{
+			printf("WHAT?! WADAFAC LINES!\n");
+			break;
+		}
+		if (tab[i] == '#')
+		{
+			printf ("ca rentre dans cette boucle\n");
+			diese_lines++;
+			printf("diese_line : %d\n", diese_lines);
+			if (tab[i] == '#' && tab[i + 1] == '#' && diese_lines <= 3)
+			{
+				printf("c'est bon mafack les lines\n");
+				return (diese_lines);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+
+int check_columns(char *tab, int fd)
+{
+	int i;
+	int diese_columns;
+
+	i = 0;
+	diese_columns = 0;
+
+	while (tab[i] != '\0')
+	{
+		if (tab[i] != '#' && tab[i] != '.')
+		{
+			printf("WHAT?! WADAFAC COLUMNS!\n");
+			break;
+		}
+		if (tab[i] == '#')
+		{
+			printf ("ca rentre dans cette boucle\n");
+			diese_columns++;
+			printf("diese_columns : %d\n", diese_columns);
+			if (tab[i] == '#' && tab[i + 5] == '#' && diese_columns <= 3)
+			{
+				printf("c'est bon mafack les columns\n");
+				return (diese_columns);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+
+int		ft_parsing(char *tab, int fd)
+{
+	int i;
+	int diese_total;
 	int ret;
 
 	i = 0;
-	diese = 0;
+	diese_total = 0;
 	ret = read(fd, tab, 545);
 	tab[ret] = '\0';
-	printf("ret : %d\n", ret);
-	printf("tab : \n%s\n", tab);
-	while (tab[i] != '\0')
+	check_lines(tab, fd);
+	check_columns(tab, fd);
+	diese_total = check_lines(tab, fd) + check_columns(tab, fd);
+	if (diese_total != 3)
 	{
-		if (tab[i] == '.')
-			i++;
-		if (tab[i] == '#')
-		{
-			printf("diese : %d\n", diese);
-			printf("i : %d\n", i);
-			printf("tab[i] :\n%c\n", tab[i]);
-			diese++;
-			i++;
-			if (tab[i + 1] == '#' || tab[i + 5] == '#')
-			{
-			}
-		}
-		if (diese > 4)
-		{
-			printf("Hello Mofo");
+		printf("INVALID PATTERN THO");
 			return (0);
-		}
 	}
 	return (0);
 }
